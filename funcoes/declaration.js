b10aad9145bab620fb6725e4196ece62e7184724
juanCristian08 <juanborba033@gmail.com>
2025-06-27 08:13:54 -0300

    trabalhando com declaration

---
+++ funcoes/declaration.js
@@ -0,0 +1,18 @@
+//função declariation - declarando uma declaração
+/**
+ 1. Palavra reservada function
+ 2. nome da função
+ 3. () parenteses
+ 4. {} chaves - dentro das chaves é o corpo (escopo) da função
+ */
+function saudacao() {
+    console.log("executando saudacao");
+}
+
+//chamando a função
+saudacao();
+
+//funcção sem retorno
+function semRetorno() {
+    console.log("essa função não retorna nada");
+}
