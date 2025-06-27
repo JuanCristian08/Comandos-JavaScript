a5b2f3f14961f24c40c46be20bfc02b6d8f283f2
juanCristian08 <juanborba033@gmail.com>
2025-06-27 08:32:29 -0300

    finalizando declaration

--- funcoes/declaration.js
+++ funcoes/declaration.js
@@ -10,9 +10,36 @@ function saudacao() {
 }
 
 //chamando a função
-saudacao();
+comRetorno();
 
 //funcção sem retorno
 function semRetorno() {
     console.log("essa função não retorna nada");
 }
+
+//função com retorno
+function comRetorno() {
+    return 1 + 1;
+}
+let resultado = comRetorno();
+console.log(comRetorno());
+
+//funções podem ter parametros
+//parametros são uma forma de injetar um valor dentro do corpo da função
+function comParametro(mensagem) {
+    console.log(mensagem);
+}
+comParametro("olá, essa é uma mensagem de saudação");
+
+ function comParametro(valor1, valor2){
+    return valor1 + valor2;
+ }
+
+ console.log(comParametro(10, 20));
+
+ //passando parametros com valores padrão
+
+ function parametrosPadrao(valor1 = 0, valor2 = 0) {
+    return valor1 + valor2;
+ }
+ console.log(parametrosPadrao(350, 150)); 
